3482f2ec992b92093796d3f8d42fa6e9bf662ab0
Will Ezell <will@dotcms.com>
2017-08-24 15:04:40 -0400

    working

--- src/main/java/com/dotcms/job/unlockcontent/UnlockContentTimer.java
+++ src/main/java/com/dotcms/job/unlockcontent/UnlockContentTimer.java
@@ -19,68 +19,74 @@ import com.liferay.portal.model.User;
 
 
 public class UnlockContentTimer implements StatefulJob {
-    public final static  String THREAD_SLEEP_BETWEEN_UNLOCKS = "THREAD_SLEEP_BETWEEN_UNLOCKS";
-    public final static  String SQL_LIMIT_CLAUSE = "SQL_LIMIT_CLAUSE";
-    public final static  String UNLOCK_AFTER_SECONDS = "UNLOCK_AFTER_SECONDS";
-    
-    
-	@Override
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-
-		Logger.info(this, "Timed Unlock: ------------------------------------------");
-
-		int seconds = (Integer) context.get(UNLOCK_AFTER_SECONDS);
-
-		Calendar cal = Calendar.getInstance();
+    public final static String THREAD_SLEEP_BETWEEN_UNLOCKS = "THREAD_SLEEP_BETWEEN_UNLOCKS";
+    public final static String SQL_LIMIT_CLAUSE = "SQL_LIMIT_CLAUSE";
+    public final static String UNLOCK_AFTER_SECONDS = "UNLOCK_AFTER_SECONDS";
 
 
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
 
-		cal.add(Calendar.SECOND, -seconds);
+        Logger.info(this, "Timed Unlock: ------------------------------------------");
+
+        int limit = Integer.parseInt(OSGiPluginProperties
+                        .getProperty(UnlockContentTimer.SQL_LIMIT_CLAUSE, "1000"));
+        int seconds = Integer.parseInt(OSGiPluginProperties
+                        .getProperty(UnlockContentTimer.UNLOCK_AFTER_SECONDS, "86400"));
+
+        long threadSleep = Integer.parseInt(OSGiPluginProperties
+                        .getProperty(UnlockContentTimer.THREAD_SLEEP_BETWEEN_UNLOCKS, "50"));
 
 
-
-		int count = 0;
-
-		try {
-			DotConnect db = new DotConnect();
-			
-		
-			int limit  = (Integer) context.get(SQL_LIMIT_CLAUSE);
-			long threadSleep  = (Long) context.get(THREAD_SLEEP_BETWEEN_UNLOCKS);
-			for(int i=0;i<1000;i++){
-				db.setSQL("select identifier, lang, working_inode from contentlet_version_info where contentlet_version_info.locked_on < ? and locked_by is not null");
-				db.setMaxRows(limit);
-				db.addParam(cal.getTime());
-				User system = APILocator.getUserAPI().getSystemUser();
-				List<Map<String, Object>> results = db.loadObjectResults();
-				if(results.size()==0)break;
-
-				for (Map<String, Object> map : results) {
-					Contentlet c = APILocator.getContentletAPI().find(map.get("working_inode").toString(), system, false);
-					APILocator.getContentletAPI().unlock(c, system, false);
-					count++;
-					Thread.sleep(threadSleep);
-				}
-				
-				Logger.info(this.getClass(), "Timed Unlock: run " + i);
-			}
-			Logger.info(this.getClass(), "Timed Unlock: Unlocked " + count + " contentlets");
-			
-			
-			
-		} catch (DotDataException e) {
-			Logger.error(this.getClass(), e.getMessage(), e);
-		} catch (DotSecurityException e) {
-			Logger.error(this.getClass(), e.getMessage(), e);
-		} catch (InterruptedException e) {
-			Logger.error(this.getClass(), e.getMessage(), e);
-			e.printStackTrace();
-		} finally {
-		    DbConnectionFactory.closeSilently();
-		}
+        Calendar cal = Calendar.getInstance();
 
 
 
-	}
+        cal.add(Calendar.SECOND, -seconds);
+
+
+
+        int count = 0;
+
+        try {
+            DotConnect db = new DotConnect();
+
+            for (int i = 0; i < 1000; i++) {
+                db.setSQL("select identifier, lang, working_inode from contentlet_version_info where contentlet_version_info.locked_on < ? and locked_by is not null");
+                db.setMaxRows(limit);
+                db.addParam(cal.getTime());
+                User system = APILocator.getUserAPI().getSystemUser();
+                List<Map<String, Object>> results = db.loadObjectResults();
+                if (results.size() == 0)
+                    break;
+
+                for (Map<String, Object> map : results) {
+                    Contentlet c = APILocator.getContentletAPI()
+                                    .find(map.get("working_inode").toString(), system, false);
+                    APILocator.getContentletAPI().unlock(c, system, false);
+                    count++;
+                    Thread.sleep(threadSleep);
+                }
+
+                Logger.info(this.getClass(), "Timed Unlock: run " + i);
+            }
+            Logger.info(this.getClass(), "Timed Unlock: Unlocked " + count + " contentlets");
+
+
+
+        } catch (DotDataException e) {
+            Logger.error(this.getClass(), e.getMessage(), e);
+        } catch (DotSecurityException e) {
+            Logger.error(this.getClass(), e.getMessage(), e);
+        } catch (InterruptedException e) {
+            Logger.error(this.getClass(), e.getMessage(), e);
+            e.printStackTrace();
+        } finally {
+            DbConnectionFactory.closeSilently();
+        }
+
+
+
+    }
 
 }
